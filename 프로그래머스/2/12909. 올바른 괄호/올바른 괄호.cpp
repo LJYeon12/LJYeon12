245aeac1ec851fe6548b6dccf531ee3d3e2ef746
LJYeon12 <102432331+LJYeon12@users.noreply.github.com>
2023-12-19 22:52:28 +0900

    [level 2] Title: 올바른 괄호, Time: 0.38 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,29 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int opened = 0;
+    int i = 0;
+    
+    while(s[i] != 0) {
+        if(s[i] == '(') {
+            opened++;
+        } else {
+            if (opened == 0) {
+                answer = false;
+                break;
+            } else {
+                opened--;
+            }
+        }
+        i++;
+    }
+    
+    if (opened != 0)
+        answer = false;
+    return answer;
+}
