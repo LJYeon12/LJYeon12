4051a7a7c985d5f03d4d8d3cec8513b1559559bc
LJYeon12 <102432331+LJYeon12@users.noreply.github.com>
2025-01-13 21:05:05 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 10956 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -3,63 +3,71 @@ using namespace std;
 #define X first
 #define Y second
 string board[1002];
-int dist1[1002][1002]; // 불의 전파 시간
-int dist2[1002][1002]; // 지훈이의 이동 시간
+int fire_dist[1002][1002]; // 불의 전파 시간
+int jihun_dist[1002][1002]; // 지훈의 이동 시간
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int n, m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  for(int i = 0; i < n; i++){
-    fill(dist1[i], dist1[i]+m, -1);
-    fill(dist2[i], dist2[i]+m, -1);    
-  }
-  for(int i = 0; i < n; i++)
-    cin >> board[i];  
-  queue<pair<int,int> > Q1;
-  queue<pair<int,int> > Q2;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(board[i][j] == 'F'){
-        Q1.push({i,j});
-        dist1[i][j] = 0;        
-      }
-      if(board[i][j] == 'J'){
-        Q2.push({i,j});
-        dist2[i][j] = 0;
-      }
-    }
-  }
-  // 불에 대한 BFS
-  while(!Q1.empty()){
-    auto cur = Q1.front(); Q1.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue; 
-      dist1[nx][ny] = dist1[cur.X][cur.Y]+1;
-      Q1.push({nx,ny});
-    }
-  }
 
-  // 지훈이에 대한 BFS
-  while(!Q2.empty()){
-    auto cur = Q2.front(); Q2.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
-        cout << dist2[cur.X][cur.Y]+1; 
-        return 0;
-      }
-      if(dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-      if(dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.X][cur.Y]+1) continue; // 불의 전파 시간을 조건에 추가
-      dist2[nx][ny] = dist2[cur.X][cur.Y]+1;
-      Q2.push({nx,ny});
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        fill(fire_dist[i], fire_dist[i] + m, -1);
+        fill(jihun_dist[i], jihun_dist[i] + m, -1);
     }
-  }
-  cout << "IMPOSSIBLE"; // 여기에 도달했다는 것은 탈출에 실패했음을 의미.
+
+    queue<pair<int, int>> fire_q;
+    queue<pair<int, int>> jihun_q;
+
+    for (int i = 0; i < n; i++) {
+        cin >> board[i];
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'F') {
+                fire_q.push({i, j});
+                fire_dist[i][j] = 0; // 불의 초기 위치
+            }
+            if (board[i][j] == 'J') {
+                jihun_q.push({i, j});
+                jihun_dist[i][j] = 0; // 지훈의 초기 위치
+            }
+        }
+    }
+
+    // 불의 BFS
+    while (!fire_q.empty()) {
+        auto cur = fire_q.front();
+        fire_q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (fire_dist[nx][ny] != -1 || board[nx][ny] == '#') continue; // 이미 방문 or 벽
+            fire_dist[nx][ny] = fire_dist[cur.X][cur.Y] + 1;
+            fire_q.push({nx, ny});
+        }
+    }
+
+    // 지훈의 BFS
+    while (!jihun_q.empty()) {
+        auto cur = jihun_q.front();
+        jihun_q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) { // 경계를 벗어나면 탈출
+                cout << jihun_dist[cur.X][cur.Y] + 1 << "\n";
+                return 0;
+            }
+            if (jihun_dist[nx][ny] != -1 || board[nx][ny] == '#') continue; // 이미 방문 or 벽
+            if (fire_dist[nx][ny] != -1 && fire_dist[nx][ny] <= jihun_dist[cur.X][cur.Y] + 1) continue; // 불이 먼저 도달
+            jihun_dist[nx][ny] = jihun_dist[cur.X][cur.Y] + 1;
+            jihun_q.push({nx, ny});
+        }
+    }
+
+    cout << "IMPOSSIBLE\n";
+    return 0;
 }
